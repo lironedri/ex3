1c10d243e24d0c6f84873644cdacb0dd739be563
dekellahav <dekelahav@gmail.com>
2018-12-21 02:08:31 +0200

    command

--- PrintCommand.cpp
+++ PrintCommand.cpp
@@ -1,5 +1,13 @@
 #include "PrintCommand.h"
 
 int PrintCommand::execute(vector<string> data, int index){
-
+    // in case we need to print string, the char " will be in the string
+    if(data[index + 1].find("\"") != string::npos){
+        cout << data[index + 1].substr(1, data[index + 1].length() - 2) << endl;
+        // in case we need to print var
+    } else{
+        ShuntingYard* varExp = new ShuntingYard();
+        cout << varExp->evaluate(data[index + 1])->calculate() << endl;
+    }
+    return index + 2;
 }
